24faf992c3df8807663bd324b9eeb61496fa5c30
MungaSoftwiz <mwachilumobm@gmail.com>
2024-02-17 18:13:13 +0300

    Update tests to handle both print and sleeping

--- mocking/countdown_two_test.go
+++ mocking/countdown_two_test.go
@@ -2,28 +2,46 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
 
-	Countdown(buffer, spySleeper)
+		Countdown(buffer, &SpyCountdownOperations{})
 
-	got := buffer.String()
+		got := buffer.String()
 	// Backticks-good way of creating str, lets you
 	// include newlines, tabs etc.
-	want := `3
+		want := `3
 2
 1
 Go!`
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
 	}
-	// Asserts the sleep has been called 3 time
-	if spySleeper.Calls != 3 {
-		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
-	}
+	})
+
 }
