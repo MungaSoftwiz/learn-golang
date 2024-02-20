e05f0fa2c9738d2ce1f22e55926115c4edcdc89f
MungaSoftwiz <mwachilumobm@gmail.com>
2024-02-21 02:34:24 +0300

    Make a configurable racer func for timeouts

--- select/v2/racer.go
+++ select/v2/racer.go
@@ -5,7 +5,14 @@ import (
 	"net/http"
 	"time"
 )
+
+var tenSecondTimeout = 10 * time.Second
+
 func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
 
 	// select helps us synchronize processes easily
 	// We now don't check for website's speed one after the other
@@ -17,7 +24,7 @@ func Racer(a, b string) (winner string, error error) {
 		return b, nil
 	// time.After() comes very handy in select
 	// It returns a chan & will send signal to it after defined time
-	case <-time.After(10 * time.Second):
+	case <-time.After(timeout):
 		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 }
