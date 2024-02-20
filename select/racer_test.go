5baa92a684819d695ba81bfc626a529bf1f2761e
MungaSoftwiz <mwachilumobm@gmail.com>
2024-02-21 00:54:02 +0300

    Refactor tests to remove duplicate code

--- select/racer_test.go
+++ select/racer_test.go
@@ -11,18 +11,13 @@ import (
 // So we use the "net/http/httptest" which enables us create a mock HTTP server
 func TestRacer(t *testing.T) {
 
-	// Handler func takes in a response writer & req
-	// This is how we write a server in Go
-	// Difference is we are wrapping it in httptest.NewServer, makes it easier to use in tests
-	// It finds an open port to listen to & we can close servers when done with tests
-	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(20 * time.Millisecond)
-		w.WriteHeader(http.StatusOK)
-	}))
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}))
+	// defer prefix will call a function at the end of a containing function
+	// Keep instruction near where you created server for easier spotting
+	defer slowServer.Close()
+	defer fastServer.Close()
 
 	slowURL := slowServer.URL //"http://www.facebook.com"
 	fastURL := fastServer.URL //before:"http://www.quii.dev"
@@ -34,6 +29,17 @@ func TestRacer(t *testing.T) {
 		t.Errorf("got %q, want %q", got, want)
 	}
 
-	slowServer.Close()
-	fastServer.Close()
+}
+
+
+
+// Handler func takes in a response writer & req
+// This is how we write a server in Go
+// Difference is we are wrapping it in httptest.NewServer, makes it easier to use in tests
+// It finds an open port to listen to & we can close servers when done with tests
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+        return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+                time.Sleep(delay)
+                w.WriteHeader(http.StatusOK)
+        }))
 }
