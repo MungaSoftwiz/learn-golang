487c97e49e908e411823dc3dc12ba708407c2a97
MungaSoftwiz <mwachilumobm@gmail.com>
2024-03-04 14:36:20 +0300

    Add code to make new counter

--- sync/sync.go
+++ sync/sync.go
@@ -1,14 +1,28 @@
 package main
 
+import (
+	"sync"
+)
 // we keep some state for the counter in our datatype and increment on every Inc call
 type Counter struct {
+	mu	sync.Mutex
 	value int
 }
 
+// any goroutine calling Inc will acquire lock on Counter if they're first
+// All other goroutines have to wait for it to Unlock before getting access
 func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.value++
 }
 
 func (c *Counter) Value() int {
 	return c.value
+}
+
+// create a constructor which shows readers of your API that
+// it would be better not to initialise the type yourself
+func NewCounter() *Counter {
+	return &Counter{}
 }
