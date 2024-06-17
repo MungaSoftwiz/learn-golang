4c41ad11b26cd6ebe4d640e94a1162f98ea13933
MungaSoftwiz <mwachilumobm@gmail.com>
2024-06-17 17:15:14 +0300

    Test contexts with derived contexts

--- context/context_test.go
+++ context/context_test.go
@@ -1,30 +1,66 @@
-package context
+package contexts
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
-type StubStore struct {
-	response string
+type SpyStore struct {
+	response  string
+	cancelled bool
 }
 
-// a method for the StubStore struct
-func (s *StubStore) Fetch() string {
+// a method for the SpyStore struct
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond) // take some time to return data
 	return s.response
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
 }
 
 func TestServer(t *testing.T) {
 	data := "hello, world"
-	svr := Server(&StubStore{data})
 
-	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder()
+	t.Run("returns data from store", func(t *testing.T) {
+		store := &SpyStore{response: data}
+		svr := Server(store)
 
-	svr.ServeHTTP(response, request)
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
 
-	if response.Body.String() != data {
-		t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
-	}
+		svr.ServeHTTP(response, request)
+
+		if response.Body.String() != data {
+			t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
+		}
+	})
+
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		store := &SpyStore{response: data}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		// Creates a context. Creates using functions like
+		// context.WithCancel, context.WithDeadline, context.WithTimeout
+		// these functions return a derived context and a cancel function
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		if !store.cancelled {
+			t.Error("store was not told to cancel")
+		}
+	})
 }
+
+/* context docs: https://go.dev/blog/context */
