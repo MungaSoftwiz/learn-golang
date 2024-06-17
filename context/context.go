8e0308fa9ecf8acc4e1860474d7f144dee502edf
MungaSoftwiz <mwachilumobm@gmail.com>
2024-06-17 17:18:45 +0300

    Add server that returns a handler func

--- context/context.go
+++ context/context.go
@@ -1,18 +1,21 @@
-package context
+package contexts
 
 import (
 	"fmt"
 	"net/http"
 )
 
+// store fetches data and cancels
 type Store interface {
 	Fetch() string
+	Cancel()
 }
-
 
 // takes in a Store and returns us a http.HandlerFunc
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// result store.Fetch() is written to http response
+		store.Cancel()
 		fmt.Fprint(w, store.Fetch())
 	}
 }
