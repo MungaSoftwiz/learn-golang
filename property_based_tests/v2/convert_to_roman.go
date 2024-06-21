cb7809e41264600379ac538ab5c1984dcb620369
Boniface Munga <122830272+MungaSoftwiz@users.noreply.github.com>
2024-06-21 08:19:40 +0000

    Update to include convert to arabic

---
+++ property_based_tests/v2/convert_to_roman.go
@@ -0,0 +1,53 @@
+package convert_roman
+
+import "strings"
+
+type RomanNumeral struct {
+	Value int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func ConvertToRoman(arabic int) string {
+	// A Builder is used to efficiently build a string using
+	// Write  & not concat. It minimizes memory copying.
+	var result strings.Builder
+
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
+	}
+
+	return result.String()
+}
+
+func ConvertToArabic(roman string) int {
+	var arabic = 0
+
+	for _, numeral := range allRomanNumerals {
+		// strings.HasPrefix checks if the string starts with the prefix
+		// and removes the prefix from the string if it does
+		for strings.HasPrefix(roman, numeral.Symbol) {
+			arabic += numeral.Value
+			roman = strings.TrimPrefix(roman, numeral.Symbol)
+		}
+	}
+	return arabic
+}
